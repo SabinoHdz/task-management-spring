035cbabe37c89f520199754c524dc0830778999f
Sabino Hernandez Paulino <sabino.hernandez@exos.mx>
2025-03-19 14:19:39 -0600

    project init

---
+++ src/test/java/com/springboot/app/project_task/project_task/ProjectTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.app.project_task.project_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
